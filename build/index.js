1a686750bdc9b2b2f4f9983f946103780c165796
Todd Garrison <toddlgarrison@gmail.com>
2023-07-26 15:22:39 -0400

    setting up sorter class and numbers array

--- build/index.js
+++ build/index.js
@@ -1,6 +1,11 @@
 "use strict";
-console.log('Hello there');
-const logSomething = () => {
-    console.log('General Kenobi');
-};
-logSomething();
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+    }
+}
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-console.log('Hello there')
+class Sorter {
+    constructor(public collection: number[]) {}
 
-const logSomething = () => {
-    console.log('General Kenobi')
+    sort():void {
+
+    }
 }
 
-logSomething();
+const sorter = new Sorter([10, 3, -5, 0])
+
+sorter.sort()
+console.log(sorter.collection);
